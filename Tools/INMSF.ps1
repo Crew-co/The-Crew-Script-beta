c49755f616da9ece12f96705163db296acb46a93
OmegaXxX <35782519+lavalxavier@users.noreply.github.com>
2020-02-05 11:33:49 -0600

    Update INMSF.ps1

--- Tools/INMSF.ps1
+++ Tools/INMSF.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -Uri http://windows.metasploit.com/metasploitframework-latest.msi -OutFile MetaSploit.exe -UseBasicParsing
+Invoke-WebRequest -Uri https://windows.metasploit.com/metasploitframework-latest.msi -OutFile MetaSploit.msi -UseBasicParsing
 cls
 cmd MetaSploit.msi
 call C:\The-Crew-Script-beta\Setup\Set2.bat
